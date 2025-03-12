2ef87ff600962067d99135ca45d8cc49eb2a6a8b
umrmaulana <maulanau147@gmail.com>
2025-03-12 15:19:40 +0700

    home activity

--- app/src/main/java/com/example/registermobile/HomeActivity.java
+++ app/src/main/java/com/example/registermobile/HomeActivity.java
@@ -1,6 +1,11 @@
 package com.example.registermobile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +14,59 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class HomeActivity extends AppCompatActivity {
+    TextView tvWelcome;
+    Button imgBtnLogout;
+    ImageButton btnEditProfile, btnProduct;
+    String email, nama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
+        getSupportActionBar().hide();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        email = getIntent().getStringExtra("email");
+        nama = getIntent().getStringExtra("nama");
+        TextView tvNama = (TextView) findViewById(R.id.tv_nama);
+        tvNama.setText(getIntent().getStringExtra("nama"));
+        TextView tvEmail = (TextView) findViewById(R.id.tv_email);
+        tvEmail.setText(getIntent().getStringExtra("email"));
+        imgBtnLogout = findViewById(R.id.imgBtnLogout);
+        btnEditProfile = findViewById(R.id.btnEditProfile);
+        btnProduct = findViewById(R.id.btnProduct);
+
+        imgBtnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        btnEditProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, MainEditProfile.class);
+                intent.putExtra("email", email);
+                intent.putExtra("nama", nama);
+                startActivity(intent);
+                finish();
+            }
+        });
+        btnEditProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, ProductActivity.class);
+                intent.putExtra("email", email);
+                intent.putExtra("nama", nama);
+                startActivity(intent);
+                finish();
+            }
         });
     }
 }
