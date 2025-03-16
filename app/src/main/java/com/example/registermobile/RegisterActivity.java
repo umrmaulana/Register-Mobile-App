f43d474299a1ed6b00ccccb90577a13d81217e42
umrmaulana <maulanau147@gmail.com>
2025-03-16 19:19:57 +0700

    update run local pc

--- app/src/main/java/com/example/registermobile/RegisterActivity.java
+++ app/src/main/java/com/example/registermobile/RegisterActivity.java
@@ -39,6 +39,7 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
+        getSupportActionBar().hide();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/registermobile/ServerAPI.java
+++ app/src/main/java/com/example/registermobile/ServerAPI.java
@@ -1,5 +1,6 @@
 package com.example.registermobile;
 
 public class ServerAPI {
+//    public String BASE_URL = "http://10.0.2.2/webservice/";
     public String BASE_URL="https://qgis.umrmaulana.my.id/webservice/";
 }
